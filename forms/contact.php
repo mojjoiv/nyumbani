38d681f6f3b098857377cf65bc5398f147f73030
Dalton <patrick.ongeche@mawingunetworks.com>
2021-01-30 01:16:07 +0300

    index update

--- forms/contact.php
+++ forms/contact.php
@@ -1,6 +1,6 @@
 <?php
  
-  $receiving_email_address = 'info@Nyumbani.co.ke';
+  $receiving_email_address = 'daltonongeche@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -17,14 +17,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'gmail.com',
+    'username' => 'dalton',
+    'password' => '54321',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
